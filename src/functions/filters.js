d62fb19cea28f70f0910335defeb8b404ae39fae
Pier4413 <pierre.delmas@delmasweb.net>
2022-07-07 22:40:17 +0200

    Update the filters to round all values

--- src/functions/filters.js
+++ src/functions/filters.js
@@ -8,7 +8,7 @@ function filterLatest(data, unit, conversionFunction, ...keys) {
         ret.push(number+unit);
       } else {
         let number = Number(latest[keys[k]]).toFixed(2)
-        ret.push(latest[keys[k]]+unit);
+        ret.push(number+unit);
       }
     }
 
